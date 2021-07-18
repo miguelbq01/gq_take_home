f1eb90d55786b5ff44e7c05e6fb0d07427449d3c
mbolanos <mbolanos@insulet.com>
2021-07-17 17:15:05 -0700

    Modify description text in ResearcherApp

--- app/javascript/components/ResearcherApp/ResearcherApp.tsx
+++ app/javascript/components/ResearcherApp/ResearcherApp.tsx
@@ -1,24 +1,27 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { getIncentives } from '@api/endpoints';
-import { IncentiveForm } from './IncentiveForm';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { getIncentives } from "@api/endpoints";
+import { IncentiveForm } from "./IncentiveForm";
+import { IncentiveTable } from "./IncentiveTable";
 
 export const ResearcherApp: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<Incentive[]>(null);
 
   useEffect(() => {
-    getIncentives()
-      .then(incentives => {
-        setData(incentives);
-        setLoading(false);
-      });
+    getIncentives().then(incentives => {
+      setData(incentives);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <div className="px-12 py-6">
       <h1 className="text-2xl font-bold mb-6">Setup incentive</h1>
-      <p className="mb-4">Enter the coupon code for the candidate to receive:</p>
+      <p className="mb-4">
+        Enter the coupon code or generate a random code for the candidate to
+        receive:
+      </p>
 
       {loading && <span>Loading...</span>}
 
