762fdf21775e53bcb4cde61ec83d9f608da0a715
mbolanos <mbolanos@insulet.com>
2021-07-17 17:22:40 -0700

    Add system tests for researcher page

--- test/system/researcher_test.rb
+++ test/system/researcher_test.rb
@@ -9,23 +9,32 @@ class ResearcherTest < ApplicationSystemTestCase
 
     it 'should_show_the_current_coupon' do
       visit '/setup'
-      assert_equal 'COUPON_123', find_field('incentive_code').value
+      assert_equal 'COUPON_123', find_by_id('code_table').first("tbody tr td").text
     end
   end
 
-  describe 'updating_incentive' do
+  describe 'add new codes' do
     let(:incentive) { create(:incentive, code: 'OLD_CODE')}
 
     setup do
       incentive ## create incentive beforehand as let doesnt run until called
     end
     
-    it 'should_update_the_code' do
+    it 'should_update_the_code_table' do
       visit '/setup'
       fill_in 'incentive_code', with: 'NEW_CODE'
       click_on 'Save'
-      assert_text 'Successfully updated'
-      assert_equal 'NEW_CODE', incentive.reload.code
+      assert_text 'Successfully created'
+      table_tr = find_by_id('code_table').all("tbody tr")
+      assert_equal 'OLD_CODE', table_tr.first.first("td").text
+      assert_equal 'NEW_CODE', table_tr.last.first("td").text
+    end
+
+    it 'should return Code already on the list, please enter another code, when sending existing code' do
+      visit '/setup'
+      fill_in 'incentive_code', with: 'OLD_CODE'
+      click_on 'Save'
+      assert_text 'Code already on the list, please enter another code'
     end
   end
 end
