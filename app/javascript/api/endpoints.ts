eee306e3f9b7791463cfe7911c0581eb15e2bf1c
mbolanos <mbolanos@insulet.com>
2021-07-17 17:17:50 -0700

    Requests to get code, update/create incentives

--- app/javascript/api/endpoints.ts
+++ app/javascript/api/endpoints.ts
@@ -1,17 +1,41 @@
-
 export const getIncentives = async (): Promise<Incentive[]> => {
-  const resp = await fetch('/api/incentives');
+  const resp = await fetch("/api/incentives");
   if (resp.ok) {
     return await resp.json();
   }
   return null;
 };
 
-export const updateIncentive = async (id: number, params: Partial<Incentive>): Promise<Incentive> => {
+export const getIncentiveCode = async (): Promise<Incentive> => {
+  const resp = await fetch("/api/get_code");
+  if (resp.ok) {
+    return await resp.json();
+  }
+  return null;
+};
+
+export const updateIncentive = async (
+  id: number,
+  params: Partial<Incentive>
+): Promise<Incentive[]> => {
   const resp = await fetch(`/api/incentives/${id}`, {
-    method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(params),
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(params)
+  });
+  if (resp.ok) {
+    return await resp.json();
+  }
+  return null;
+};
+
+export const createIncentive = async (
+  params: Partial<Incentive>
+): Promise<Incentive[]> => {
+  const resp = await fetch(`/api/incentives`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(params)
   });
   if (resp.ok) {
     return await resp.json();
