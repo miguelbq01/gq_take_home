22bb416fa3573baaa2cb1875bf01a384fc9730f8
mbolanos <mbolanos@insulet.com>
2021-07-17 17:21:09 -0700

    Add getIncentiveCode request to Redeem.tsx

--- app/javascript/components/CandidateApp/Redeem.tsx
+++ app/javascript/components/CandidateApp/Redeem.tsx
@@ -1,21 +1,28 @@
-import * as React from 'react';
-import { useState } from 'react';
+import * as React from "react";
+import { useState } from "react";
+import { getIncentiveCode } from "@api/endpoints";
 
-interface Props {
-  data: Incentive[];
-}
-export const Redeem: React.FC<Props> = ({ data }) => {
+export const Redeem: React.FC = () => {
   const [redeemed, setRedeemed] = useState(false);
+  const [noCodeAvailable, setNoCodeAvailable] = useState(false);
+  const [selected, setSelected] = useState("");
 
   async function handleClickRedeem() {
-    setRedeemed(true);
+    setRedeemed(false);
+    setNoCodeAvailable(false);
+    const incentive = await getIncentiveCode();
+    if (incentive) {
+      setRedeemed(true);
+      setSelected(incentive.code);
+    } else {
+      setNoCodeAvailable(true);
+    }
   }
 
   return (
     <div>
       <div className="pb-4">
         <button
-          disabled={redeemed}
           className="hover:bg-gray-100 bg-gray-200 rounded-md px-4 py-2"
           onClick={handleClickRedeem}
         >
@@ -25,7 +32,12 @@ export const Redeem: React.FC<Props> = ({ data }) => {
 
       {redeemed && (
         <div className="py-4 text-green-600 italic">
-          Your code is: {data[0].code}. Thanks for participating in our research!
+          Your code is: {selected}. Thanks for participating in our research!
+        </div>
+      )}
+      {noCodeAvailable && (
+        <div className="py-4 text-red-600 italic">
+          There are no more codes available!
         </div>
       )}
     </div>
