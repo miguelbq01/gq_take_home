7347453db1cf93108305e0c2e023a263ffff9976
mbolanos <mbolanos@insulet.com>
2021-07-17 17:17:22 -0700

    Add get_code method to obtain random code, method to create new incentive, add redeemed to permit params

--- app/controllers/api/incentives_controller.rb
+++ app/controllers/api/incentives_controller.rb
@@ -6,16 +6,38 @@ class Api::IncentivesController < ApplicationController
     render json: @incentives.to_json
   end
 
+  def get_code
+    @incentives = Incentive.unredeemed
+    if @incentives.size > 0
+      random_incentive = @incentives[rand(1..@incentives.size) - 1]
+      random_incentive.update(redeemed: true)
+      render json: random_incentive.to_json
+    else
+      render json: { error: "No codes available." }, status: :not_found
+    end
+  end
+
   def update
     @incentive = Incentive.find(params[:id])
-
-    @incentive.update!(update_params)
+    @incentive.update!(incentive_params)
     render json: @incentive.to_json
+  end
+
+  def create
+    if Incentive.create(code: incentive_params[:code]).valid?
+      render json: incentives.to_json
+    else
+      render json: { error: 'Code already created' }, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def update_params
-    params.require(:incentive).permit(:code)
+  def incentive_params
+    params.require(:incentive).permit(:code, :redeemed)
+  end
+
+  def incentives
+    @_incentives ||= Incentive.all
   end
 end
